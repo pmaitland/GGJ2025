81ef1268bfd75645a1fff733bcbe107684a321e3
Iain Cattermole <22656572+Iain530@users.noreply.github.com>
2025-01-25 21:24:19 +0000

    Bubbal Hud (#22)
    * fix things
    
    * add game end and game reset screens
    
    * fixed width timer
    
    * bubble-spawner
    
    * fix timer colour on retry
    
    ---------
    
    Co-authored-by: Noel Rajan <leonr13@hotmail.co.uk>
    Co-authored-by: pmaitland <33162414+pmaitland@users.noreply.github.com>

---
+++ ggj25/scripts/bubble_spawner.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@onready var timer: Timer = $Timer
+@export var bubble_scene: PackedScene
+
+enum SPAWNER_TYPE { MANUAL, TIMER }
+
+@export var spawn_mode: SPAWNER_TYPE = SPAWNER_TYPE.MANUAL
+@export var spawn_delay: float = 10
+
+func spawn_bubble():
+	var bubble: Bubble = bubble_scene.instantiate()
+
+
+func _ready() -> void:
+	if spawn_mode == SPAWNER_TYPE.TIMER:
+		timer.wait_time = spawn_delay
+		timer.start()
+
+
+func _on_timer_timeout() -> void:
+	spawn_bubble()
