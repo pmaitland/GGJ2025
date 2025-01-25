ff9e4f3e0cafe229cdfa4ddf55205e7a4070ab32
Noel Rajan <leonr13@hotmail.co.uk>
2025-01-25 15:01:20 +0000

    add basic main screen

---
+++ ggj25/scripts/main_screen.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var game_mode: Button = $VBoxContainer/GameMode
+@onready var controls: Button = $VBoxContainer/Controls
+@onready var settings: Button = $VBoxContainer/Settings
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
