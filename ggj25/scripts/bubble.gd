1b3ae2473fe462decc04bc3272fc42eb915560bd
Iain Cattermole <iain_c@live.co.uk>
2025-01-25 00:19:34 +0000

    bubball

--- ggj25/scripts/bubble.gd
+++ ggj25/scripts/bubble.gd
@@ -2,15 +2,18 @@ class_name Bubble extends CharacterBody2D
 
 
 const FRICTION = 0.985
-
+const BOUNCE_SCALE = 0.8
 
 func blow(from: Transform2D, force: float) -> void:
 	var direction = (global_transform.origin - from.origin).normalized()
 	velocity += direction * force
 
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	
 	velocity *= FRICTION
 	
-	move_and_slide()
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		velocity = velocity.bounce(collision_info.get_normal())
+	
