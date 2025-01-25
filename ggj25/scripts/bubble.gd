94a2731de18be89c9e1b124f1fd41cbefbb987c8
Iain Cattermole <22656572+Iain530@users.noreply.github.com>
2025-01-25 13:32:05 +0000

    evil obstacle (#10)

--- ggj25/scripts/bubble.gd
+++ ggj25/scripts/bubble.gd
@@ -16,4 +16,12 @@ func _physics_process(delta: float) -> void:
 	var collision_info = move_and_collide(velocity * delta)
 	if collision_info:
 		velocity = velocity.bounce(collision_info.get_normal())
-	
+
+
+func pop():
+	# play pop animation then delete
+	queue_free()
+
+func collect():
+	# sound / animation then delete
+	queue_free()
--- ggj25/scripts/obstacle.gd
+++ ggj25/scripts/obstacle.gd
@@ -3,5 +3,5 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Bubble:
-		print('bubble collected')
-		(body as Bubble).collect()
+		print(name, ' hit bubble')
+		(body as Bubble).pop()
