971c1f3097751b6af578f74a87a1204486ad033a
Iain Cattermole <22656572+Iain530@users.noreply.github.com>
2025-01-25 14:53:05 +0000

    Bubbalgame (#14)
    * complete merge
    
    * its a game!!

--- ggj25/scripts/goal.gd
+++ ggj25/scripts/goal.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
 
+@export var team_id: int = 0
+
+signal bubble_collected
+
 func _on_body_entered(body: Node2D) -> void:
 	if body is Bubble:
 		print('bubble collected')
 		(body as Bubble).collect()
+		bubble_collected.emit(team_id)
