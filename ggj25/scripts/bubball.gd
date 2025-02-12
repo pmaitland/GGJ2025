cb48b30eac8e192ec3d31996ec911d0d38158c2c
Iain Cattermole <iain_c@live.co.uk>
2025-02-12 21:21:47 +0000

    overtime bubbal

--- ggj25/scripts/bubball.gd
+++ ggj25/scripts/bubball.gd
@@ -2,6 +2,8 @@ extends GameMode
 
 @onready var bubble_spawner: BubbleSpawner = $BubbleSpawner
 
+var overtime = false
+var overtime_timer: float = 0
 
 func _on_hud_start_game() -> void:
 	start_game()
@@ -17,11 +19,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	super._process(delta)
-	# add game specific code here
+	if overtime and !paused:
+		overtime_timer += delta
 
 
 func _on_game_timer_timeout() -> void:
-	finish_game()
+	if is_tie():
+		start_overtime()
+	else:
+		finish_game()
 
 
 func _on_left_goal_bubble_collected(_bubble: Bubble) -> void:
@@ -34,6 +40,25 @@ func _on_right_goal_bubble_collected(_bubble: Bubble) -> void:
 		on_goal_scored(LEFT_TEAM)
 
 
+func start_overtime():
+	for bubble in bubbles:
+		if bubble:
+			bubble.pop()
+	
+	enable_duck_input(false)
+	hud.show_message("Tie!")
+	await get_tree().create_timer(1.0).timeout
+	hud.show_message("Overtime!")
+	await get_tree().create_timer(1.0).timeout
+	reset_positions_and_start(true)
+	
+
+func format_timer(time_left: float):
+	if overtime:
+		return super.format_timer(floor(overtime_timer))
+	return super.format_timer(time_left)
+
+
 func on_goal_scored(team_id: int):
 	hud.show_scores_message(get_team_name(team_id))
 	add_score(team_id)
@@ -42,6 +67,15 @@ func on_goal_scored(team_id: int):
 		if duck != null:
 			duck.goal_scored()
 	
+	if overtime:
+		update_scoreboard()
+		finish_game()
+	else:
+		reset_positions_and_start()
+
+
+
+func reset_positions_and_start(is_overtime: bool = false) -> void:
 	game_timer.set_paused(true)
 
 	enable_duck_input(false)
@@ -49,7 +83,7 @@ func on_goal_scored(team_id: int):
 	
 	if !paused:
 		hud.show_message("Get Ready!")
-	# Reset duck positions
+
 	reset_duck_positions()
 	bubbles = [bubble_spawner.spawn_bubble()]
 	await get_tree().create_timer(1.0).timeout
@@ -59,8 +93,11 @@ func on_goal_scored(team_id: int):
 		return
 
 	hud.show_message("Go!")
-	game_timer.set_paused(false)
+	if is_overtime:
+		overtime = true
+	else:
+		game_timer.set_paused(false)
 	enable_duck_input(true)
-		
+
 	await get_tree().create_timer(0.6).timeout
 	hud.hide_message()
